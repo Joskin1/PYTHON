918c93adb1b6f46421ad58da45ba083d90b3ba94
joskin1 <akinjoseph221@gmail.com>
2025-09-22 19:44:14 +0100

    If statement

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 age = int(input("Enter your age: "))
 if age >= 18:
     print("You are eligible to vote.")
+elif age == 17:
+        print("You are almost eligible to vote.")   
 else:
     print("You are not eligible to vote.")
 # End if
