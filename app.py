beb6fc80d1e10266e828767bd020b3d3947c7887
joskin1 <akinjoseph221@gmail.com>
2025-09-22 21:20:39 +0100

    Weight converter

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-operator = input("Enter an operator (+, -, *, /): ")
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
+weight = float(input("Enter your weight: "))
+unit =  input("Kilogram or POunds.... (K or P): ").upper()
 
-if operator == "+":
-    result = float(num1) + float(num2)
-    print(f"{num1} + {num2} = {result}")
-elif operator == "-":
-    result = float(num1) - float(num2)
-    print(f"{num1} - {num2} = {result}")
-elif operator == "*":
-    result = float(num1) * float(num2)
-    print(f"{num1} * {num2} = {result}")
-elif operator == "/":
-    if float(num2) == 0:
-        print("Error: Division by zero is not allowed.")
-    else:
-        result = float(num1) / float(num2)
-        print(f"{num1} / {num2} = {result}")
+if unit == "K":
+    weight = weight * 2.20462
+    unit = "Lbs"
+    print(f"Your weight is {round(weight, 2)} {unit}")
+elif unit == "P":
+    weight = weight / 2.20462
+    unit = "Kg"
+    print(f"Your weight is {round(weight, 2)} {unit}")
 else:
-    print("Invalid operator. Please use one of +, -, *, or /.") 
+    print("Invalid unit")
